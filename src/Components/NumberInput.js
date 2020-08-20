037e131ba09ac0a90899498396c0b49e5c74b614
betchieph <elizabeth.voyagessouth.org>
2020-08-20 22:06:10 +0000

    finishedinputfidget

---
+++ src/Components/NumberInput.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+class NumberInput extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      number: null
+    }
+  };
+    // Be sure to add a state.
+  handleChange = (event) => {  
+  this.setState({number: event.target.value});
+  }
+
+  render() {
+    return(
+      <li className="NumberInput cards-item">
+            <div className="card">
+              <div className="card-content">
+                <div className="card-title">Number</div>
+                <p className="card-text">
+                  <div className="input btn-block">
+                    <input type="number" onChange={this.handleChange} className="textInput" />
+                  </div>
+                  <div className="output">
+                    <label for="numberOutput">State: </label>
+                    <span name="numberOutput">{this.state.number}</span>
+                  </div>
+            </p>
+          </div>
+        </div>
+      </li>
+    )
+  }
+}
+    
+  
+
+
+export default NumberInput;
